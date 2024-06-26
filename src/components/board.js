d3c746e471305f420ad75a38c29198b45ae4bf99
Annika Eigemann <annika.eigemann@studium.fernuni-hagen.de>
2024-06-25 22:52:14 -0700

    Add board notation

--- src/components/board.js
+++ src/components/board.js
@@ -3,16 +3,47 @@ import React from 'react';
 import '../index.css';
 import Square from './square.js';
 
+const  defaultStyle = {
+  fontSize: "10px",
+  textAlign: "left",
+}
+
 export default class Board extends React.Component {
 
-  renderSquare(i, squareShade) {
+  
+
+  renderSquare(i, squareShade, value) {
+
+    value = this.getValueBasedOnSquareIdx(i)
     return <Square
       key={i}
-      keyVal={i}
-      style={this.props.squares[i] ? this.props.squares[i].style : null}
+      keyVal={value}
+      style={this.props.squares[i] ? this.props.squares[i].style : defaultStyle}
       shade={squareShade}
       onClick={() => this.props.onClick(i)}
     />
+  }
+
+  getValueBasedOnSquareIdx(i) {
+    if(i === 0) return "8"
+    if(i === 10) return "7"
+    if(i === 20) return "6"
+    if(i === 30) return "5"
+    if(i === 40) return "4"
+    if(i === 50) return "3"
+    if(i === 60) return "2"
+
+    if(i === 70) return "1 a"
+
+    if(i === 71) return "b"
+    if(i === 72) return "c"
+    if(i === 73) return "d"
+    if(i === 74) return "e"
+    if(i === 75) return "f"
+    if(i === 76) return "g"
+    if(i === 77) return "h"
+    if(i === 78) return "i"
+    if(i === 79) return "j"
   }
 
   render() {
--- src/pieces/piece.js
+++ src/pieces/piece.js
@@ -2,7 +2,7 @@ export default class Piece {
   constructor(player, name, iconUrl) {
     this.player = player;
     this.name = name;
-    this.style = { backgroundImage: "url('" + iconUrl + "')" };
+    this.style = { backgroundImage: "url('" + iconUrl + "')", fontSize: "10px", textAlign: "left"};
   }
 
   getPlayer() {
